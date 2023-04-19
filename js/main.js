cc1ebbf46b3ee83a5eec2862e7cee36c075191a7
Milouyg <33333@ma-web.nl>
2023-04-19 16:14:57 +0200

    Added images and converted to webp. Added json in the rightsection instead of placing dummy text

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ class Main {
     happinessMain;
     happinessRightSection;
     happinessLeftSection;
-
+    firstEpisode;
 
     constructor(placeToRenderMain, episodes) {
         this.placeToRenderMain = document.getElementsByTagName(placeToRenderMain)[0];
@@ -65,8 +65,8 @@ class Main {
         this.happinessMain = document.createElement("main");
         this.happinessMain.classList = "happiness";
 
-        this.happinessRightSection = new RightSection(this.happinessMain, episodes);
         this.happinessLeftSection = new LeftSection(this.happinessMain, this, episodes);
+        this.happinessRightSection = new RightSection(this.happinessMain, this.firstEpisode);
     }
 
     render() {
@@ -78,108 +78,6 @@ class Main {
     changeRightSection(clickedEpisode) {
         this.happinessRightSection.changeRightSectionContent(clickedEpisode);
     }
-}
-
-class RightSection {
-    placeToRender;
-    happinessRightSection;
-    detailUl;
-    detailLi;
-
-    detailImg;
-    detailPDate;
-    detailPTitle;
-    detailPText;
-
-    detailGroup;
-    detailDownload;
-    detailLink;
-
-    constructor(placeToRender) {
-        this.placeToRender = placeToRender;
-
-        this.happinessRightSection = document.createElement("section");
-        this.happinessRightSection.classList = "happiness__section happiness__section--right";
-
-        this.detailUl = document.createElement("ul");
-        this.detailUl.classList = "detail__ul";
-
-        // All li's
-        this.detailLiTop = document.createElement("li");
-        this.detailLiTop.classList = "detail__li";
-
-        this.detailLiMiddle = document.createElement("li");
-        this.detailLiMiddle.classList = "detail__li";
-
-        this.detailBottom = document.createElement("li");
-        this.detailBottom.classList = "detail__li";
-        // --------------------
-
-        this.detailImg = document.createElement("img");
-        this.detailImg.classList = "detail__img";
-        this.detailImg.setAttribute("src", "https://via.placeholder.com/600x200");
-        this.detailImg.setAttribute("alt", "")  //   <----- Don't forget
-
-        this.detailPDate = document.createElement("p");
-        this.detailPDate.classList = "detail__p detail__p--date";
-        this.detailPDate.innerText = "date";
-
-        this.detailPTitle = document.createElement("p");
-        this.detailPTitle.classList = "detail__p detail__p--title";
-        this.detailPTitle.innerText = "title";
-
-        this.detailPText = document.createElement("p");
-        this.detailPText.classList = "detail__p detail__p--summary";
-        this.detailPText.innerText = "Hier moet Json data gepakt worden";
-
-        this.detailGroup = document.createElement("div");
-        this.detailGroup.classList = "detail__group";
-
-        this.detailDownload = document.createElement("button");
-        this.detailDownload.classList = "detail__download";
-        this.detailDownload.innerText = "Downloaden";
-
-        this.detailLink = document.createElement("a");
-        this.detailLink.classList = "detail__link";
-        this.detailLink.innerText = "Source >";
-        this.detailLink.setAttribute("href", "url");    //   <----- Don't forget
-        this.detailLink.setAttribute("target", "_blank");
-    }
-
-    render() {
-        this.placeToRender.appendChild(this.happinessRightSection);
-        this.happinessRightSection.appendChild(this.detailUl);
-        // Li top
-        this.detailUl.appendChild(this.detailLiTop);
-        this.detailLiTop.appendChild(this.detailImg);
-        this.detailLiTop.appendChild(this.detailPDate);
-        this.detailLiTop.appendChild(this.detailPTitle);
-        // Li middle
-        this.detailUl.appendChild(this.detailLiMiddle);
-        this.detailLiMiddle.appendChild(this.detailPText);
-
-        this.detailUl.appendChild(this.detailBottom);
-        this.detailBottom.appendChild(this.detailGroup);
-        this.detailGroup.appendChild(this.detailDownload);
-        this.detailGroup.appendChild(this.detailLink);
-    }
-
-    changeRightSectionContent(clickedEpisode) {
-        this.detailImg.setAttribute("src", clickedEpisode["image"]["src"]);
-        this.detailImg.setAttribute("alt", clickedEpisode["image"]["alt"]);
-
-        this.detailPDate.innerText = clickedEpisode["date (dd-mm-yyyy)"];
-        this.detailPTitle.innerText = clickedEpisode["title"];
-
-        this.detailDownload.addEventListener("click", () => {
-            window.location = clickedEpisode["audio"];
-        });
-
-        this.detailLink.setAttribute("href", clickedEpisode["url"]);
-    }
-
-
-
 }
 
 class LeftSection {
@@ -212,7 +110,7 @@ class LeftSection {
         for (let i = 0; i < 4; i++) {
             let randomNumber = this.randomizer();
             if(i === 0){
-                this.happinessClassMain.changeRightSection(this.episodes[randomNumber]);
+                this.happinessClassMain.firstEpisode = this.episodes[randomNumber];
             }
 
             // The number may not be there twice
@@ -224,9 +122,7 @@ class LeftSection {
 
             this.happinessLi = document.createElement("li");
             this.happinessLi.classList = "happiness__li";
-            this.happinessLi.addEventListener("click", () => {
-                this.happinessClassMain.changeRightSection(this.episodes[randomNumber]);
-            })
+            this.happinessLi.addEventListener("click", () => this.happinessClassMain.changeRightSection(this.episodes[randomNumber]));
 
             this.happinessImg = document.createElement("img");
             this.happinessImg.classList = "happiness__img";
@@ -249,12 +145,110 @@ class LeftSection {
     }
 
     randomizer() {
-        return Math.floor(Math.random() * (this.episodes.length - 1));
+        return Math.floor(Math.random() * this.episodes.length);
     }
 
     render() {
         this.placeToRender.appendChild(this.happinessLeftSection);
         this.happinessLeftSection.appendChild(this.happinessUl);
+    }
+}
+
+class RightSection {
+    placeToRender;
+    happinessRightSection;
+    detailUl;
+    detailLi;
+
+    detailImg;
+    detailPDate;
+    detailPTitle;
+    detailPText;
+
+    detailGroup;
+    detailDownload;
+    detailLink;
+
+    constructor(placeToRender, firstEpisode) {
+        this.placeToRender = placeToRender;
+
+        this.happinessRightSection = document.createElement("section");
+        this.happinessRightSection.classList = "happiness__section happiness__section--right";
+
+        this.detailUl = document.createElement("ul");
+        this.detailUl.classList = "detail__ul";
+
+        // All li's
+        this.detailLiTop = document.createElement("li");
+        this.detailLiTop.classList = "detail__li";
+
+        this.detailLiMiddle = document.createElement("li");
+        this.detailLiMiddle.classList = "detail__li";
+
+        this.detailBottom = document.createElement("li");
+        this.detailBottom.classList = "detail__li";
+        // --------------------
+
+        this.detailImg = document.createElement("img");
+        this.detailImg.classList = "detail__img";
+        this.detailImg.setAttribute("src", firstEpisode["image"]["src"]);
+        this.detailImg.setAttribute("alt", "")  //   <----- Don't forget
+
+        this.detailPDate = document.createElement("p");
+        this.detailPDate.classList = "detail__p detail__p--date";
+        this.detailPDate.innerText = firstEpisode["date (dd-mm-yyyy)"];
+
+        this.detailPTitle = document.createElement("p");
+        this.detailPTitle.classList = "detail__p detail__p--title";
+        this.detailPTitle.innerText = firstEpisode["title"];
+
+        this.detailPText = document.createElement("p");
+        this.detailPText.classList = "detail__p detail__p--summary";
+        this.detailPText.innerText = firstEpisode["summary"];
+
+        this.detailGroup = document.createElement("div");
+        this.detailGroup.classList = "detail__group";
+
+        this.detailDownload = document.createElement("button");
+        this.detailDownload.classList = "detail__download";
+        this.detailDownload.innerText = "Downloaden";
+        this.detailDownload.addEventListener("click", () => window.location = firstEpisode["audio"]);
+
+        this.detailLink = document.createElement("a");
+        this.detailLink.classList = "detail__link";
+        this.detailLink.innerText = "Source >";
+        this.detailLink.setAttribute("href", firstEpisode["url"]);    //   <----- Don't forget
+        this.detailLink.setAttribute("target", "_blank");
+    }
+
+    render() {
+        this.placeToRender.appendChild(this.happinessRightSection);
+        this.happinessRightSection.appendChild(this.detailUl);
+        // Li top
+        this.detailUl.appendChild(this.detailLiTop);
+        this.detailLiTop.appendChild(this.detailImg);
+        this.detailLiTop.appendChild(this.detailPDate);
+        this.detailLiTop.appendChild(this.detailPTitle);
+        // Li middle
+        this.detailUl.appendChild(this.detailLiMiddle);
+        this.detailLiMiddle.appendChild(this.detailPText);
+
+        this.detailUl.appendChild(this.detailBottom);
+        this.detailBottom.appendChild(this.detailGroup);
+        this.detailGroup.appendChild(this.detailDownload);
+        this.detailGroup.appendChild(this.detailLink);
+    }
+
+    changeRightSectionContent(clickedEpisode) {
+        this.detailImg.setAttribute("src", clickedEpisode["image"]["src"]);
+        this.detailImg.setAttribute("alt", clickedEpisode["image"]["alt"]);
+
+        this.detailPDate.innerText = clickedEpisode["date (dd-mm-yyyy)"];
+        this.detailPTitle.innerText = clickedEpisode["title"];
+
+        this.detailDownload.addEventListener("click", () => window.location = clickedEpisode["audio"]);
+
+        this.detailLink.setAttribute("href", clickedEpisode["url"]);
     }
 }
 
